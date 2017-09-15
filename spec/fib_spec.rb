457fb78d43d29b78d8db724348dbdb0b6e014e2f
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-09-15 13:28:59 +0000

    track changes in fib_spec.rb

--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -1,9 +1,1 @@
 require 'spec_helper'
-
-RSpec.describe Fib, type: :model do
-  describe 'calculate_fib should work' do
-    it 'should return the nth digit of the Fibonacci sequence' do
-      expect(Fib(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    end
-  end
-end
