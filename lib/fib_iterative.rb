3a629a2bebf9eea5ef6e87ab7f41a34362280d0f
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-09-15 11:14:15 +0000

    writing solutions

---
+++ lib/fib_iterative.rb
@@ -0,0 +1,17 @@
+def fibI(n)
+  if n == 0 || n == 1
+    return n
+  else
+    fib0 = 0
+    fib1 = 1
+    fib = 1
+    (n-1).times do |f|
+      fib = fib0 + fib1
+      fib0 = fib1
+      fib1 = fib
+    end
+    fib
+  end
+end
+
+# puts fibI(9)
